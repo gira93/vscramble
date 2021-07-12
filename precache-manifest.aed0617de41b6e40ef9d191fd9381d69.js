f1b6e00df3dd07528627bd77778000afd7ce2a71
gira93 <23706223+gira93@users.noreply.github.com>
2021-07-12 17:14:45 +0200

    gh-pages

---
+++ precache-manifest.aed0617de41b6e40ef9d191fd9381d69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2954c88c12afa4082380",
+    "url": "/vscramble/css/app.3ebad986.css"
+  },
+  {
+    "revision": "66893fb199fb30c8080bb6410572e552",
+    "url": "/vscramble/index.html"
+  },
+  {
+    "revision": "2954c88c12afa4082380",
+    "url": "/vscramble/js/app.09955697.js"
+  },
+  {
+    "revision": "cfb2e615c862cb4ed54c",
+    "url": "/vscramble/js/chunk-vendors.ba0ea71d.js"
+  },
+  {
+    "revision": "0b655e890b89c6e6e522c4c6895f1273",
+    "url": "/vscramble/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vscramble/robots.txt"
+  }
+]);
